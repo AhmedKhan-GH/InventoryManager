cec370c018570a9d532b983eaf7df4c530d7cc7b
AhmedKhan-GH <emailahmedebadkhan@gmail.com>
2023-11-27 01:13:25 -0800

    user schema update

--- main.cpp
+++ main.cpp
@@ -9,16 +9,17 @@ int main()
     database.createTableIfNotExists
     (
         "Users",
-        "user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "user_name TEXT UNIQUE NOT NULL CHECK(length(user_name) <= 32),"
-        "user_salt TEXT UNIQUE NOT NULL CHECK(length(user_salt) <= 32),"
-        "user_passhash TEXT UNIQUE NOT NULL CHECK(length(user_passhash) <= 40),"
-        "user_timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-        "user_visibility BOOLEAN NOT NULL DEFAULT 1,"
-        "user_legalname TEXT CHECK(length(user_legalname) <= 64),"
-        "user_phonenumber TEXT CHECK(length(user_phonenumber) <= 16),"
-        "user_emailaddress TEXT CHECK(length(user_emailaddress) <= 256),"
-        "user_description TEXT,"
-        "user_permission TEXT NOT NULL DEFAULT 'BASE' CHECK(user_permission IN ('LOCK', 'BASE', 'SUPER', 'ADMIN'))"
+        "user_id            INTEGER     PRIMARY KEY     AUTOINCREMENT, "
+        "user_name          TEXT        NOT NULL        UNIQUE,"
+        "user_salt          TEXT        NOT NULL, "
+        "user_passhash      TEXT        NOT NULL, "
+        "user_legalname     TEXT, "
+        "user_phonenumber   TEXT, "
+        "user_emailaddress  TEXT, "
+        "user_description   TEXT, "
+        "user_permission    INTEGER     NOT NULL        DEFAULT 1, "
+        "user_visibility    BOOLEAN     NOT NULL        DEFAULT 1, "
+        "user_timestamp     DATETIME    NOT NULL        DEFAULT CURRENT_TIMESTAMP"
     );
+
 }
